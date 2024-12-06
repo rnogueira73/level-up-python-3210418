72445ba446b7de51649c5549f752e5847d14c90e
rnogueira73 <ruben_nogueira@hotmail.com>
2024-12-06 14:25:53 -0600

    Test of code in github

--- src/03 Sort a String/sort_wordsR.py
+++ src/03 Sort a String/sort_wordsR.py
@@ -1,6 +1,5 @@
 def sort_words(string):
-    pass
-
+    return " ".join(sorted(string.split(), key=len))
 
 if __name__ == '__main__':
     print(sort_words('banana ORANGE apple'))  # apple banana ORANGE
